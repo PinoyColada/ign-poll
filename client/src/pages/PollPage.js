423cfbfc955cae6fca50b4ecc63f1ae36a0118d2
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-04-29 21:19:02 -0400

    styled poll layout and button

--- client/src/pages/PollPage.js
+++ client/src/pages/PollPage.js
@@ -21,14 +21,15 @@ const PollPage = () => {
     <div>
       <h1>This is the poll page</h1>
       <div>
-          {polls.map((poll) => (
-            <div key={poll.id} className='pollContainer'>
-              <h3 className='pollTitle'>{poll.question}</h3>
-              <img className='pollImg'src={poll.img} alt="Poll"/>
-              <Link to={`/${poll.id}`} state={{ id: poll.id }}><button> View Options </button></Link>
-            </div>
-          ))}
-        </div>
+        {polls.map((poll) => (
+          <div key={poll.id} className='pollContainer'>
+            <h3 className='pollTitle'>{poll.question}</h3>
+            <img className='pollImg' src={poll.img} alt="Poll" />
+            <Link to={`/${poll.id}`} state={{ id: poll.id, title: poll.title }}><button className='pollBtn'> View Choices </button></Link>
+          </div>
+        ))}
+      </div>
+      <h3>Don't see a poll you're looking for? Create a poll now by simply logging in!</h3>
     </div>
   )
 }
