6a95fbf23c251202d971727cf7b388eba9b5101f
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-04-30 17:05:56 -0400

    changed voteCount to vote_count in seed file

--- seeders/20220428182349-demo-options.js
+++ seeders/20220428182349-demo-options.js
@@ -4,19 +4,19 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('options', [{
       choice: 'The game can do better',
-      voteCount: 21,
+      vote_count: 21,
       poll_id: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       choice: 'The best Halo game out of all the Halo games!',
-      voteCount: 47,
+      vote_count: 47,
       poll_id: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       choice: 'Never played the game.',
-      voteCount: 30,
+      vote_count: 30,
       poll_id: 1,
       createdAt: new Date(),
       updatedAt: new Date()
